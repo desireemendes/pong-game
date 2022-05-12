af7f0cce4f365f17c5875170421b7b5f927d569c
Desiree Mendes <desireefrost@live.ca>
2022-05-12 18:17:01 -0400

    Create paddle and positioning

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
 
+
+# width = 20
+# height = 100
+# x_pos = 350
+# y_pos = 0
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+def go_up():
+  new_y = paddle.ycor() + 20
+  paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+  new_y = paddle.ycor() - 20
+  paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(go_up, "up")
+screen.onkey(go_down, "down")
+
 screen.exitonclick()
+# setposition(350, 0)
